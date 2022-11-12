a50a3bbe4817c8a0d172d20d582851f188b1666f
Ahmed Farid <ahmedfarid272@gmail.com>
2022-11-12 23:25:48 +0200

    Add files via upload

---
+++ LIB/BIT_MATH.h
@@ -0,0 +1,17 @@
+/**************************************************/
+/* Author    : Ahmed Farid Hafez                  */
+/* Layer     : Library                            */
+/* Component : BIT MATH                           */
+/* Date      : 15 FEB 2022                        */
+/* Version   : V01                                */
+/**************************************************/
+#ifndef _BIT_MATH_H
+#define _BIT_MATH_H
+
+#define SET_BIT(VAR,BITNO) (VAR) |=  (1 << (BITNO))
+#define CLR_BIT(VAR,BITNO) (VAR) &= ~(1 << (BITNO))
+#define TOG_BIT(VAR,BITNO) (VAR) ^=  (1 << (BITNO))
+#define GET_BIT(VAR,BITNO) (((VAR) >> (BITNO)) & 0x01)
+
+
+#endif
